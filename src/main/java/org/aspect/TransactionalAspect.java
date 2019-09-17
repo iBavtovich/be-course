eb3ac89ccee894a73af2dea3c0a4595ab629d1df
Ignat Bavtovich <ignatbav@gmail.com>
2019-09-17 23:35:56 +0300

    Switched to Spring AOP with CGLib proxies creation mechanism

---
+++ src/main/java/org/aspect/TransactionalAspect.java
@@ -0,0 +1,27 @@
+package org.aspect;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.transactional.JdbcConnectionHolder;
+
+@RequiredArgsConstructor
+public class TransactionalAspect {
+
+    private final JdbcConnectionHolder connectionHolder;
+
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        Object result;
+        try {
+            connectionHolder.startTransaction();
+            result = pjp.proceed();
+            connectionHolder.commitTransaction();
+        } catch (Exception e) {
+            connectionHolder.rollbackTransaction();
+            throw new RuntimeException(e);
+        } finally {
+            connectionHolder.closeConnection();
+        }
+        return result;
+    }
+
+}
